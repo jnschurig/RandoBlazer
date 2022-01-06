384616a35ab205ae90ea874abc899aec5891952c
jschurig <jnschurig@gmail.com>
2022-01-06 15:56:17 -0700

    Converting more of the data. Still a ways to go on this front.

---
+++ reference/chests.py
@@ -0,0 +1,312 @@
+'''    
+List of chest contents:
+
+     - Medical Herb x13         - Mole's Ribbon
+     - Strange Bottle x5        - Big Pearl
+     - Sword of Life            - Mermaid's Tears
+     - Critical Sword           - Delicious Seeds
+     - Lucky Blade              - Emblem B
+     - Zantetsu Sword           - Emblem D
+     - Spirit Sword             - Red-Hot Ball
+     - Soul Blade               - Power Bracelet
+     - Iron Armor               - 1 Gem
+     - Ice Armor                - 12 Gems
+     - Magic Armor              - 40 Gems
+     - Light Armor              - 50 Gems x5
+     - Soul Armor               - 60 Gems x2
+     - Rotator                  - 80 Gems x5
+     - Flame Pillar             - 100 Gems x4
+     - Tornado                  - 150 Gems
+     - Dream Rod                - 200 Gems
+     - Leo's Brush              - Nothing x3
+
+    Total: 66 chests
+'''
+
+CHESTS = [
+	{
+		"chest_id": 0,
+		"item_id": "SWORD_OF_LIFE"
+	},
+	{
+		"chest_id": 1,
+		"item_id": "IRON_ARMOR"
+	},
+	{
+		"chest_id": 2,
+		"item_id": "GEMS_EXP",
+		"gem_count": 50
+	},
+	{
+		"chest_id": 3,
+		"item_id": "GEMS_EXP",
+		"gem_count": 12
+	},
+	{
+		"chest_id": 4,
+		"item_id": "MEDICAL_HERB"
+	},
+	{
+		"chest_id": 5,
+		"item_id": "DREAM_ROD"
+	},
+	{
+		"chest_id": 6,
+		"item_id": "LEOS_BRUSH"
+	},
+	{
+		"chest_id": 7,
+		"item_id": "MEDICAL_HERB"
+	},
+	{
+		"chest_id": 8,
+		"item_id": "TORNADO"
+	},
+	{
+		"chest_id": 9,
+		"item_id": "ICE_ARMOR"
+	},
+	{
+		"chest_id": 10,
+		"item_id": "MOLES_RIBBON"
+	},
+	{
+		"chest_id": 11,
+		"item_id": "GEMS_EXP",
+		"gem_count": 50
+	},
+	{
+		"chest_id": 12,
+		"item_id": "GEMS_EXP",
+		"gem_count": 150
+	},
+	{
+		"chest_id": 13,
+		"item_id": "MEDICAL_HERB"
+	},
+	{
+		"chest_id": 14,
+		"item_id": "DELICIOUS_SEEDS"
+	},
+	{
+		"chest_id": 15,
+		"item_id": "GEMS_EXP",
+		"gem_count": 50
+	},
+	{
+		"chest_id": 16,
+		"item_id": "MEDICAL_HERB"
+	},
+	{
+		"chest_id": 17,
+		"item_id": "NOTHING"
+	},
+	{
+		"chest_id": 18,
+		"item_id": "STRANGE_BOTTLE"
+	},
+	{
+		"chest_id": 19,
+		"item_id": "GEMS_EXP",
+		"gem_count": 100
+	},
+	{
+		"chest_id": 20,
+		"item_id": "GEMS_EXP",
+		"gem_count": 60
+	},
+	{
+		"chest_id": 21,
+		"item_id": "FLAME_PILLAR"
+	},
+	{
+		"chest_id": 22,
+		"item_id": "MERMAIDS_TEARS"
+	},
+	{
+		"chest_id": 23,
+		"item_id": "BIG_PEARL"
+	},
+	{
+		"chest_id": 24,
+		"item_id": "EMBLEM_D"
+	},
+	{
+		"chest_id": 25,
+		"item_id": "GEMS_EXP",
+		"gem_count": 80
+	},
+	{
+		"chest_id": 26,
+		"item_id": "MEDICAL_HERB"
+	},
+	{
+		"chest_id": 27,
+		"item_id": "NOTHING"
+	},
+	{
+		"chest_id": 28,
+		"item_id": "MEDICAL_HERB"
+	},
+	{
+		"chest_id": 29,
+		"item_id": "MEDICAL_HERB"
+	},
+	{
+		"chest_id": 30,
+		"item_id": "GEMS_EXP",
+		"gem_count": 60
+	},
+	{
+		"chest_id": 31,
+		"item_id": "CRITICAL_SWORD"
+	},
+	{
+		"chest_id": 32,
+		"item_id": "STRANGE_BOTTLE"
+	},
+	{
+		"chest_id": 33,
+		"item_id": "GEMS_EXP",
+		"gem_count": 1
+	},
+	{
+		"chest_id": 34,
+		"item_id": "POWER_BRACELET"
+	},
+	{
+		"chest_id": 35,
+		"item_id": "GEMS_EXP",
+		"gem_count": 40
+	},
+	{
+		"chest_id": 36,
+		"item_id": "MAGIC_ARMOR"
+	},
+	{
+		"chest_id": 37,
+		"item_id": "NOTHING"
+	},
+	{
+		"chest_id": 38,
+		"item_id": "MEDICAL_HERB"
+	},
+	{
+		"chest_id": 39,
+		"item_id": "GEMS_EXP",
+		"gem_count": 50
+	},
+	{
+		"chest_id": 40,
+		"item_id": "LUCKY_BLADE"
+	},
+	{
+		"chest_id": 41,
+		"item_id": "STRANGE_BOTTLE"
+	},
+	{
+		"chest_id": 42,
+		"item_id": "ROTATOR"
+	},
+	{
+		"chest_id": 43,
+		"item_id": "ZANTETSU_SWORD"
+	},
+	{
+		"chest_id": 44,
+		"item_id": "LIGHT_ARMOR"
+	},
+	{
+		"chest_id": 45,
+		"item_id": "GEMS_EXP",
+		"gem_count": 50
+	},
+	{
+		"chest_id": 46,
+		"item_id": "MEDICAL_HERB"
+	},
+	{
+		"chest_id": 47,
+		"item_id": "GEMS_EXP",
+		"gem_count": 80
+	},
+	{
+		"chest_id": 48,
+		"item_id": "STRANGE_BOTTLE"
+	},
+	{
+		"chest_id": 49,
+		"item_id": "MEDICAL_HERB"
+	},
+	{
+		"chest_id": 50,
+		"item_id": "GEMS_EXP",
+		"gem_count": 80
+	},
+	{
+		"chest_id": 51,
+		"item_id": "SPIRIT_SWORD"
+	},
+	{
+		"chest_id": 52,
+		"item_id": "STRANGE_BOTTLE"
+	},
+	{
+		"chest_id": 53,
+		"item_id": "MEDICAL_HERB"
+	},
+	{
+		"chest_id": 54,
+		"item_id": "GEMS_EXP",
+		"gem_count": 100
+	},
+	{
+		"chest_id": 55,
+		"item_id": "EMBLEM_B"
+	},
+	{
+		"chest_id": 56,
+		"item_id": "GEMS_EXP",
+		"gem_count": 80
+	},
+	{
+		"chest_id": 57,
+		"item_id": "GEMS_EXP",
+		"gem_count": 80
+	},
+	{
+		"chest_id": 58,
+		"item_id": "GEMS_EXP",
+		"gem_count": 100
+	},
+	{
+		"chest_id": 59,
+		"item_id": "MEDICAL_HERB"
+	},
+	{
+		"chest_id": 60,
+		"item_id": "MEDICAL_HERB"
+	},
+	{
+		"chest_id": 61,
+		"item_id": "GEMS_EXP",
+		"gem_count": 200
+	},
+	{
+		"chest_id": 62,
+		"item_id": "RED_HOT_BALL"
+	},
+	{
+		"chest_id": 63,
+		"item_id": "SOUL_ARMOR"
+	},
+	{
+		"chest_id": 64,
+		"item_id": "SOUL_BLADE"
+	},
+	{
+		"chest_id": 65,
+		"item_id": "GEMS_EXP",
+		"gem_count": 100
+	}
+]
