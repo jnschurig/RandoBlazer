df27f1b747ab3cf4ecf1b22774f5bc7ced383f67
James <jnschurig@gmail.com>
2022-01-17 22:48:14 -0700

    Starting a "checks" file. I think it will allow for better randomization logic, eventually...

---
+++ reference/checks.py
@@ -0,0 +1,864 @@
+check_list = [
+	{
+		"check_id": "ITEM_TOOL_SHOP_OWNER",
+		"item_id": "MEDICAL_HERB",
+		"check_type": "NPC",
+		"requirements": ['NPC_TOOL_SHOP_OWNER']
+	},
+	{
+		"check_id": "ITEM_EMBLEM_A",
+		"item_id": "EMBLEM_A",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_GOAT_PEN",
+		"item_id": "MEDICAL_HERB",
+		"check_type": "NPC",
+		"requirements": ['bridge guard', 'goat with pen']
+	},
+	{
+		"check_id": "ITEM_TEDDY",
+		"item_id": "GOATS_FOOD",
+		"check_type": "NPC",
+		"requirements": ['NPC_TEDDY']
+	},
+	{
+		"check_id": "ITEM_PASS",
+		"item_id": "PASS",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_SECRET_CAVE_TILE",
+		"item_id": "STRANGE_BOTTLE",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_VILLAGE_CHIEF",
+		"item_id": "BROWN_STONE",
+		"check_type": "NPC",
+		"requirements": ['NPC_VILLAGE_CHIEF']
+	},
+	{
+		"check_id": "ITEM_MAGICIAN",
+		"item_id": "FLAME_BALL",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_CRYSTAL_RECOVERY_SWORD",
+		"item_id": "RECOVERY_SWORD",
+		"check_type": "NPC",
+		"requirements": ['recovery sword crystal', 'pass', 'NPC_IVY_RECOVERY_SWORD']
+	},
+	{
+		"check_id": "ITEM_BIRD_RED_HOT_MIRROR",
+		"item_id": "RED_HOT_MIRROR",
+		"check_type": "NPC",
+		"requirements": ['NPC_BIRD_RED_HOT_MIRROR'],
+        "ACT": 2
+	},
+	{
+		"check_id": "ITEM_CRYSTAL_MAGIC_BELL",
+		"item_id": "MAGIC_BELL",
+		"check_type": "NPC",
+		"requirements": ['NPC_CROCODILE3', 'NPC_DEER_MAGIC_BELL', 'ITEM_EMBLEM_A', 'ITEM_EMBLEM_B', 'ITEM_EMBLEM_C', 'ITEM_EMBLEM_D', 'ITEM_EMBLEM_E', 'ITEM_EMBLEM_F', 'ITEM_EMBLEM_G', 'ITEM_EMBLEM_H'],
+        "ACT": 2
+	},
+	{
+		"check_id": "ITEM_WOODSTIN_TRIO",
+		"item_id": "MEDICAL_HERB",
+		"check_type": "NPC",
+		"requirements": ['game squirrel', 'game dog', 'game deer'],
+        "ACT": 2
+	},
+	{
+		"check_id": "ITEM_GREENWOODS_GUARDIAN",
+		"item_id": "GREEN_STONE",
+		"check_type": "NPC",
+		"requirements": ['NPC_GREENWOODS_GUARDIAN'],
+        "ACT": 2
+	},
+	{
+		"check_id": "ITEM_GREENWOOD_LEAVES",
+		"item_id": "GREENWOOD_LEAF",
+		"check_type": "NPC",
+		"requirements": [],
+        "ACT": 2
+	},
+	{
+		"check_id": "ITEM_MOLE_SHIELD_BRACELET",
+		"item_id": "SHIELD_BRACELET",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_SQUIRREL_PSYCHO_SWORD",
+		"item_id": "PSYCHO_SWORD",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_SQUIRREL_EMBLEM_C",
+		"item_id": "EMBLEM_C",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_WATER_SHRINE_TILE",
+		"item_id": "STRANGE_BOTTLE",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_CRYSTAL_LIGHT_ARROW",
+		"item_id": "LIGHT_ARROW",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_MOUNTAIN_KING",
+		"item_id": "PHOENIX",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_BOY_MUSHROOM_SHOES",
+		"item_id": "MUSHROOM_SHOES",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_NOME",
+		"item_id": "SILVER_STONE",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_SNAIL_EMBLEM_E",
+		"item_id": "EMBLEM_E",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_EMBLEM_F",
+		"item_id": "EMBLEM_F",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_EMBLEM_G",
+		"item_id": "EMBLEM_G",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_CHEST_OF_DRAWERS_MYSTIC_ARMOR",
+		"item_id": "MYSTIC_ARMOR",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_PLANT_HERB",
+		"item_id": "MEDICAL_HERB",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_CAT_DOOR_KEY",
+		"item_id": "DOOR_KEY",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_PLANT_ACTINIDIA_LEAVES",
+		"item_id": "ACTINIDIA_LEAF",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_CHEST_OF_DRAWERS_HERB",
+		"item_id": "MEDICAL_HERB",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_MARIE",
+		"item_id": "PURPLE_STONE",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_MOUSE_SPARK_BOMB",
+		"item_id": "SPARK_BOMB",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_SOLDIER_ELEMENTAL_MAIL",
+		"item_id": "ELEMENTAL_MAIL",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_SUPER_BRACELET",
+		"item_id": "SUPER_BRACELET",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_QUEEN_MAGRIDD",
+		"item_id": "VIP_CARD",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_SOLDIER_PLATINUM_CARD",
+		"item_id": "PLATINUM_CARD",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_MAID_HERB",
+		"item_id": "MEDICAL_HERB",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_EMBLEM_H",
+		"item_id": "EMBLEM_H",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_KING_MAGRIDD",
+		"item_id": "BLACK_STONE",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_DR_LEO",
+		"item_id": "MOBILE_KEY",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_HARP_STRING",
+		"item_id": "HARP_STRING",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_MERMAID_HERB",
+		"item_id": "MEDICAL_HERB",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_MERMAID_BUBBLE_ARMOR",
+		"item_id": "BUBBLE_ARMOR",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_MERMAID_MAGIC_FLARE",
+		"item_id": "MAGIC_FLARE",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_MERMAID_QUEEN",
+		"item_id": "BLUE_STONE",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_MERMAID_RED_HOT_STICK",
+		"item_id": "RED_HOT_STICK",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_LUE",
+		"item_id": "THUNDER_RING",
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_CRYSTAL_GRASS_VALLEY",
+		"item_id": "GEMS_EXP",
+		"amount": 80,
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_CRYSTAL_UNDERGROUND_CASTLE",
+		"item_id": "GEMS_EXP",
+		"amount": 30,
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_CRYSTAL_LOST_MARSH",
+		"item_id": "GEMS_EXP",
+		"amount": 150,
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_CRYSTAL_WATER_SHRINE",
+		"item_id": "GEMS_EXP",
+		"amount": 180,
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_CRYSTAL_FIRE_SHRINE",
+		"item_id": "GEMS_EXP",
+		"amount": 1,
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_CRYSTAL_MOUNTAIN_OF_SOULS",
+		"item_id": "GEMS_EXP",
+		"amount": 300,
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_CRYSTAL_LUNE",
+		"item_id": "GEMS_EXP",
+		"amount": 400,
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_CRYSTAL_LEOS_LAB_BASEMENT",
+		"item_id": "GEMS_EXP",
+		"amount": 300,
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_CRYSTAL_MODEL_TOWN",
+		"item_id": "GEMS_EXP",
+		"amount": 300,
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_CRYSTAL_POWER_PLANT",
+		"item_id": "GEMS_EXP",
+		"amount": 300,
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_CRYSTAL_ROCKBIRD",
+		"item_id": "GEMS_EXP",
+		"amount": 200,
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_CRYSTAL_SEABED_NEAR_BLESTER",
+		"item_id": "GEMS_EXP",
+		"amount": 300,
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "ITEM_CRYSTAL_SEABED_NEAR_DUREAN",
+		"item_id": "GEMS_EXP",
+		"amount": 250,
+		"check_type": "NPC",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_0",
+		"chest_id": 0,
+		"item_id": "SWORD_OF_LIFE",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_1",
+		"chest_id": 1,
+		"item_id": "IRON_ARMOR",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_2",
+		"chest_id": 2,
+		"item_id": "GEMS_EXP",
+		"amount": 50,
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_3",
+		"chest_id": 3,
+		"item_id": "GEMS_EXP",
+		"amount": 12,
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_4",
+		"chest_id": 4,
+		"item_id": "MEDICAL_HERB",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_5",
+		"chest_id": 5,
+		"item_id": "DREAM_ROD",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_6",
+		"chest_id": 6,
+		"item_id": "LEOS_BRUSH",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_7",
+		"chest_id": 7,
+		"item_id": "MEDICAL_HERB",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_8",
+		"chest_id": 8,
+		"item_id": "TORNADO",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_9",
+		"chest_id": 9,
+		"item_id": "ICE_ARMOR",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_10",
+		"chest_id": 10,
+		"item_id": "MOLES_RIBBON",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_11",
+		"chest_id": 11,
+		"item_id": "GEMS_EXP",
+		"amount": 50,
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_12",
+		"chest_id": 12,
+		"item_id": "GEMS_EXP",
+		"amount": 150,
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_13",
+		"chest_id": 13,
+		"item_id": "MEDICAL_HERB",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_14",
+		"chest_id": 14,
+		"item_id": "DELICIOUS_SEEDS",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_15",
+		"chest_id": 15,
+		"item_id": "GEMS_EXP",
+		"amount": 50,
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_16",
+		"chest_id": 16,
+		"item_id": "MEDICAL_HERB",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_17",
+		"chest_id": 17,
+		"item_id": "NOTHING",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_18",
+		"chest_id": 18,
+		"item_id": "STRANGE_BOTTLE",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_19",
+		"chest_id": 19,
+		"item_id": "GEMS_EXP",
+		"amount": 100,
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_20",
+		"chest_id": 20,
+		"item_id": "GEMS_EXP",
+		"amount": 60,
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_21",
+		"chest_id": 21,
+		"item_id": "FLAME_PILLAR",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_22",
+		"chest_id": 22,
+		"item_id": "MERMAIDS_TEARS",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_23",
+		"chest_id": 23,
+		"item_id": "BIG_PEARL",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_24",
+		"chest_id": 24,
+		"item_id": "EMBLEM_D",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_25",
+		"chest_id": 25,
+		"item_id": "GEMS_EXP",
+		"amount": 80,
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_26",
+		"chest_id": 26,
+		"item_id": "MEDICAL_HERB",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_27",
+		"chest_id": 27,
+		"item_id": "NOTHING",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_28",
+		"chest_id": 28,
+		"item_id": "MEDICAL_HERB",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_29",
+		"chest_id": 29,
+		"item_id": "MEDICAL_HERB",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_30",
+		"chest_id": 30,
+		"item_id": "GEMS_EXP",
+		"amount": 60,
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_31",
+		"chest_id": 31,
+		"item_id": "CRITICAL_SWORD",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_32",
+		"chest_id": 32,
+		"item_id": "STRANGE_BOTTLE",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_33",
+		"chest_id": 33,
+		"item_id": "GEMS_EXP",
+		"amount": 1,
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_34",
+		"chest_id": 34,
+		"item_id": "POWER_BRACELET",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_35",
+		"chest_id": 35,
+		"item_id": "GEMS_EXP",
+		"amount": 40,
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_36",
+		"chest_id": 36,
+		"item_id": "MAGIC_ARMOR",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_37",
+		"chest_id": 37,
+		"item_id": "NOTHING",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_38",
+		"chest_id": 38,
+		"item_id": "MEDICAL_HERB",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_39",
+		"chest_id": 39,
+		"item_id": "GEMS_EXP",
+		"amount": 50,
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_40",
+		"chest_id": 40,
+		"item_id": "LUCKY_BLADE",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_41",
+		"chest_id": 41,
+		"item_id": "STRANGE_BOTTLE",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_42",
+		"chest_id": 42,
+		"item_id": "ROTATOR",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_43",
+		"chest_id": 43,
+		"item_id": "ZANTETSU_SWORD",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_44",
+		"chest_id": 44,
+		"item_id": "LIGHT_ARMOR",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_45",
+		"chest_id": 45,
+		"item_id": "GEMS_EXP",
+		"amount": 50,
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_46",
+		"chest_id": 46,
+		"item_id": "MEDICAL_HERB",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_47",
+		"chest_id": 47,
+		"item_id": "GEMS_EXP",
+		"amount": 80,
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_48",
+		"chest_id": 48,
+		"item_id": "STRANGE_BOTTLE",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_49",
+		"chest_id": 49,
+		"item_id": "MEDICAL_HERB",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_50",
+		"chest_id": 50,
+		"item_id": "GEMS_EXP",
+		"amount": 80,
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_51",
+		"chest_id": 51,
+		"item_id": "SPIRIT_SWORD",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_52",
+		"chest_id": 52,
+		"item_id": "STRANGE_BOTTLE",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_53",
+		"chest_id": 53,
+		"item_id": "MEDICAL_HERB",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_54",
+		"chest_id": 54,
+		"item_id": "GEMS_EXP",
+		"amount": 100,
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_55",
+		"chest_id": 55,
+		"item_id": "EMBLEM_B",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_56",
+		"chest_id": 56,
+		"item_id": "GEMS_EXP",
+		"amount": 80,
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_57",
+		"chest_id": 57,
+		"item_id": "GEMS_EXP",
+		"amount": 80,
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_58",
+		"chest_id": 58,
+		"item_id": "GEMS_EXP",
+		"amount": 100,
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_59",
+		"chest_id": 59,
+		"item_id": "MEDICAL_HERB",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_60",
+		"chest_id": 60,
+		"item_id": "MEDICAL_HERB",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_61",
+		"chest_id": 61,
+		"item_id": "GEMS_EXP",
+		"amount": 200,
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_62",
+		"chest_id": 62,
+		"item_id": "RED_HOT_BALL",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_63",
+		"chest_id": 63,
+		"item_id": "SOUL_ARMOR",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_64",
+		"chest_id": 64,
+		"item_id": "SOUL_BLADE",
+		"check_type": "CHEST",
+		"requirements": []
+	},
+	{
+		"check_id": "CHEST_65",
+		"chest_id": 65,
+		"item_id": "GEMS_EXP",
+		"amount": 100,
+		"check_type": "CHEST",
+		"requirements": []
+	}
+
+]
